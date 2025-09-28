f0675123ba69518607e0168705a9a4b6fee5d5a9
phillipdelphias <phillipdelphias@gmail.com>
2025-09-28 15:31:31 +1000

    Verify ownership of website for adsense

--- src/lib/scripts/primaryScripts.tsx
+++ src/lib/scripts/primaryScripts.tsx
@@ -9,7 +9,7 @@ export default function PrimaryScripts() {
                     <>
                         <Script defer src="https://cloud.umami.is/script.js" data-website-id="56d5663b-96f8-4cc8-9a00-7016cc30a9f8" strategy='afterInteractive'></Script>
                         <Script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "46c80f28914946c984176d3e1a314971"}' strategy='afterInteractive'></Script>
-                       <Script async src="https://www.googletagmanager.com/gtag/js?id=G-GYCV526F5B" strategy='afterInteractive'></Script>
+                        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-GYCV526F5B" strategy='afterInteractive'></Script>
                         <Script id="google-analytics" strategy='afterInteractive'>
                             {
                                 `                                window.dataLayer = window.dataLayer || [];
@@ -20,6 +20,8 @@ export default function PrimaryScripts() {
                             }
                         </Script>
                     </> : null}
+            <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9758035810696915"
+                            crossOrigin="anonymous" strategy='afterInteractive'></Script>
 
         </>
     )
